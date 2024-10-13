96c08a3147d2650e8ef7ac7eaf2cd2590abcd4fa
piyushpal121 <rockybalboaa09@gmail.com>
2024-10-14 01:30:52 +0530

    new commit

--- FSD_JS/Events/script.js
+++ FSD_JS/Events/script.js
@@ -1,8 +1,6 @@
-document.getElementById('btn').addEventListener('click', () => {
-    document.getElementById('btn-space').innerText = `Button Clicked!`;
-    
-})
-document.getElementById('selectbox').addEventListener('onfocus', () => {
-    document.getElementById('text-space').innerText = `${Event}`;
-    
-})
+document.getElementById("btn").addEventListener("click", () => {
+  document.getElementById("btn-space").innerText = `Button Clicked!`;
+});
+document.getElementById("selectbox").addEventListener("onfocus", () => {
+  document.getElementById("text-space").innerText = `${Event}`;
+});
