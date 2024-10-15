da70156ea9e42cb157656bb4c1d62dab4da55a43
piyushpal121 <rockybalboaa09@gmail.com>
2024-10-15 14:12:30 +0530

    second commit

--- FSD_JS/Events/script.js
+++ FSD_JS/Events/script.js
@@ -4,3 +4,8 @@ document.getElementById("btn").addEventListener("click", () => {
 document.getElementById("selectbox").addEventListener("onfocus", () => {
   document.getElementById("text-space").innerText = `${Event}`;
 });
+
+let para = document.createElement("p")
+para.textContent = "------CONTENT--------";
+para.id = "para";
+document.body.appendChild(para)
